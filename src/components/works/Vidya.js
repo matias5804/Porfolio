cd8e561779a9823d27f15484713d5111986371e0
Matias Alonso <matiasalosno88@gmail.com>
2022-06-07 12:30:26 -0300

    act

--- src/components/works/Vidya.js
+++ src/components/works/Vidya.js
@@ -17,7 +17,7 @@ const Vidya = () => {
           <p>Vidya is a project that was born with the intention of raising awareness through greater understanding about sustainability and care for the environment on a daily basis. It not only seeks to market ecological and sustainable products but also aims to inform and teach users about the different habits that can be adopted, from consumption to recycling, to encourage societies to be more respectful of the environment and more aware of what they consume.</p>
 
           <div className='btnsWorks'>
-            <a href='hhttps://matias5804.github.io/VidyaPatong/' target="_blank" rel='noreferrer'><button>Visit the Site</button></a> 
+            <a href='https://matias5804.github.io/VidyaPatong/' target="_blank" rel='noreferrer'><button>Visit the Site</button></a> 
             <a href='https://github.com/matias5804/VidyaPatong.git' target="_blank" rel='noreferrer'><button>See GitHub</button></a> 
           </div>
         </div>
